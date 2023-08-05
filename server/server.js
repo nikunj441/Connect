a6b103c121ffa7f8702cef69688ec746fe86d8c9
Nikunj Manhas <nikunjmanhas441@gmail.com>
2023-08-05 12:26:21 +0530

    Deleted irrelevant comments

--- server/server.js
+++ server/server.js
@@ -8,13 +8,6 @@ import bodyParser from 'body-parser'
 import Connection from './database/db.js'
 import Router from './routes/route.js'
 
-// import path from 'path'
-// import { fileURLToPath } from 'url'
-
-
-//esmodule fix
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname =  path.dirname(__filename)
 dotenv.config()
 
 const app = express()
@@ -26,12 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/', Router)     
 
-// app.use(express.static(path.join(__dirname, './client/build')))
 
-// app.use('*', function(req,res){
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"))
-// })
-// const PORT = process.env.PORT || 8000
 const PORT = 8000;
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
